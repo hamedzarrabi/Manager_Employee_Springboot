caf57c09db51ff71c772d258a33c6112f1a4fa42
hamed zarrabi <47312982+hamedzarrabi@users.noreply.github.com>
2022-11-27 14:35:33 +0330

    STEP 4 - Complete Exception Handling

---
+++ src/main/java/com/hami/exception/domain/UserNotfoundException.java
@@ -0,0 +1,7 @@
+package com.hami.exception.domain;
+
+public class UserNotfoundException extends Exception{
+    public UserNotfoundException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/hami/resource/UserResource.java
+++ src/main/java/com/hami/resource/UserResource.java
@@ -1,16 +1,20 @@
 package com.hami.resource;
 
 import com.hami.domain.User;
+import com.hami.exception.domain.ExceptionHandling;
+import com.hami.exception.domain.UserNotfoundException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping( "/user")
-public class UserResource {
+@RequestMapping( path = {"/","/user"})
+public class UserResource extends ExceptionHandling {
 
     @GetMapping("/home")
-    public String showUser() {
-        return "application works";
+    public String showUser() throws UserNotfoundException {
+//        return "application works";
+        throw new UserNotfoundException("The user was not found");
     }
+
 }
