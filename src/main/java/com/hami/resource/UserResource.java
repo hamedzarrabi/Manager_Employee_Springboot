3b0fb0f1ed78de7382aa80fb21ecd66b744ad357
hamed zarrabi <47312982+hamedzarrabi@users.noreply.github.com>
2022-11-28 15:26:40 +0330

    STEP 6 - Generate JWT

--- src/main/java/com/hami/resource/UserResource.java
+++ src/main/java/com/hami/resource/UserResource.java
@@ -1,31 +1,61 @@
 package com.hami.resource;
 
 import com.hami.domain.User;
+import com.hami.domain.UserPrincipal;
 import com.hami.exception.ExceptionHandling;
 import com.hami.exception.domain.EmailExistException;
 import com.hami.exception.domain.UserNotfoundException;
 import com.hami.exception.domain.UsernameExistException;
 import com.hami.service.UserService;
+import com.hami.utility.JWTTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
+
+import static com.hami.constans.SecurityConstant.JWT_TOKEN_HEADER;
 
 @RestController
 @RequestMapping(path = { "/", "/user"})
 public class UserResource extends ExceptionHandling {
 
     private UserService userService;
+    private AuthenticationManager authenticationManager;
+    private JWTTokenProvider jwtTokenProvider;
 
     @Autowired
-    public UserResource(UserService userService) {
+    public UserResource(UserService userService, AuthenticationManager authenticationManager, JWTTokenProvider jwtTokenProvider) {
         this.userService = userService;
+        this.authenticationManager = authenticationManager;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     @PostMapping("/register")
-    public ResponseEntity<User> register(@RequestBody User user) throws EmailExistException, UsernameExistException, UserNotfoundException {
+    public ResponseEntity<User> register(@RequestBody User user) throws EmailExistException, UserNotfoundException, UsernameExistException {
        User newUser =  userService.register(user.getFirstName(), user.getLastName(), user.getUsername(), user.getEmail());
        return new ResponseEntity<>(newUser, HttpStatus.OK);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<User> login(@RequestBody User user) {
+       authentication(user.getUsername(), user.getPassword());
+       User loginUser = userService.findUserByUsername(user.getUsername());
+        UserPrincipal userPrincipal = new UserPrincipal(loginUser);
+        HttpHeaders jwtHeader = getJwtHeader(userPrincipal);
+       return new ResponseEntity<>(loginUser, jwtHeader, HttpStatus.OK);
+    }
+
+    private HttpHeaders getJwtHeader(UserPrincipal user) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(JWT_TOKEN_HEADER, jwtTokenProvider.generateJwtToken(user));
+        return headers;
+    }
+
+    private void authentication(String username, String password) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
     }
 
 }
